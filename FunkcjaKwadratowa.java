74b890c7f7a1d9c0b83d1923630c2780f8602647
pdzanek <justfushi@gmail.com>
2018-04-09 22:04:30 +0200

    final version

--- FunkcjaKwadratowa.java
+++ FunkcjaKwadratowa.java
@@ -4,7 +4,7 @@ public class FunkcjaKwadratowa{
 public static void main(String[] args)
 	{
 		if(args.length == 0){
-			System.out.println("-----Input Error - usage: java FunkcjaKwadratowa a b c");
+			System.out.println("Input Error - usage: java FunkcjaKwadratowa a b c");
 			System.exit(1);
 		}
 		if(args.length <3){
@@ -14,39 +14,50 @@ public static void main(String[] args)
 			System.out.println("Error: Too much arguments");
 			System.exit(1);
 		}
+		double a[] = new double[3];
 		try{
-			a = Integer.parseInteger(args[0]);
-			b = Integer.parseInteger(args[1]);
-			c = Integer.parseInteger(args[2]);
+				for(int i = 0; i<3; i++){
+       			a[i] = Double.parseDouble(args[i]);
+				System.out.println("Argument " + (i+1) + " = " + a[i]);
+				}
+			}
+		catch(NumberFormatException c){
+			System.out.println("Podany argument nie jest liczbą");
+			System.exit(1);
 		}
-		System.out.println("A=" +a[0]);
-		System.out.println("B=" +a[1]);
-		System.out.println("C=" +a[2]); 		
-		double x,x1,x2;
-
+		miejscaZerowe(a);
+	}
+public static void 	miejscaZerowe(double a[])
+{
+	double rozwiazanie[] = new double[2];
 		if(a[0]==0){
-			x=-a[2]/a[1];
-			System.out.println("Funkcja liniowa: miejsce zerowe=" +x);
-		}
+			rozwiazanie[0]=-a[2]/a[1];
+			System.out.println("Funkcja liniowa: miejsce zerowe=" +rozwiazanie[0]);
+			}
 		else{
 			double delta=a[1]*a[1]-4*a[0]*a[2];
 			System.out.println("Delta=" +delta);
 			if(delta==0){
-				x=-a[1]/2*a[0];
+				rozwiazanie[0]=-a[1]/2*a[0];
+				System.out.println("x=" +rozwiazanie[0]);
 			}
 			else if(delta>0){
-				x1=(sqrt(delta)+a[1])/(2*a[0]);
-				x2=(sqrt(delta)-a[1])/(2*a[0]);
-				System.out.println("x1=" +x1);
-				System.out.println("x1=" +x2);
+				rozwiazanie[0]=(sqrt(delta)+a[1])/(2*a[0]);
+				rozwiazanie[1]=(sqrt(delta)-a[1])/(2*a[0]);
+				System.out.println("x1=" +rozwiazanie[0]);
+				System.out.println("x2=" +rozwiazanie[1]);
 			}
 			else{
 				System.out.println("Rownanie nie ma rozwiazania w dziedzinie rzeczywistej.");
-				//TODO 2 - obliczyc w dziedzinie zespolonej
+				delta=-delta;
+				rozwiazanie[0] = (-a[1]) / (2*a[0]);
+				rozwiazanie[1] = sqrt(delta) / (2*a[0]);
+				System.out.println("Bieguny:");
+				System.out.printf("x1 = %.2f + %.2fi\n", rozwiazanie[0], rozwiazanie[1]); 
+				System.out.printf("x2 = %.2f + %.2fi\n", rozwiazanie[0], -rozwiazanie[1]);
+				}
 			}
-		}
-	}
 }
-
+}
 
 /*Napisz program czytajacy argumenty wywolania programu i konwertujacy je do Integer. Zmodyfikuj program w ten sposob, aby kolejne argumenty byly wspolczynikami wielomianu postaci Ax2+Bx+C=0. Policz pierwiastki tego wielomianu. */
